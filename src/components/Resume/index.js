cb904936242549bab9c7470228d4cc38df9a2668
lafry5 <lafry5@yahoo.com>
2020-10-13 12:28:43 -0700

    fixed resume error

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -5,7 +5,7 @@ function Resume() {
     <section className="my-5">
       <div className="my-2">
       <p>
-        Download my <a href="https://www.linkedin.com/">resume</a>
+        Download my <a href="https://www.linkedin.com/in/lauraafry">resume</a>
         </p>
         <h3>Front-end Proficiencies</h3>
         <ul className="skills">
@@ -27,6 +27,7 @@ function Resume() {
         </ul>
         <h3>Tool Proficiencies</h3>
         <ul className="skills"></ul>
+        <ul>
         <li>Git, GitHub</li>
         <li>Heroku</li>
         </ul>
